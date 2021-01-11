dfc23347f0e6e2c1a5098d1af508a1016459eb58
Igor Napierala <igor.napierala@cognifide.com>
2021-01-11 19:39:15 +0100

    some functions and component of navbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,39 @@
 <script>
-  export let name = "John Doe";
+  import Navbar from "./Navbar.svelte";
+
+  let name = "John Doe";
+  let points = 100;
+  let showControls = false;
+
+  const addPoint = () => (points += 1);
+  const removePoint = () => (points -= 1);
+  const toggleControls = () => (showControls = !showControls);
 </script>
 
 <style>
   h1 {
-    color: red;
+    color: #204f6e;
+  }
+
+  h3 {
+    margin-bottom: 10px;
   }
 </style>
 
-<main>
-  <h1>Hello {name}!</h1>
-</main>
+<Navbar />
+<div class="container">
+  <div class="card">
+    <h1>
+      {name}
+      <button class="brn btn sm" on:click={toggleControls}>
+        {#if showControls}-{:else}+{/if}
+      </button>
+    </h1>
+    <h3>Points: {points}</h3>
+    {#if showControls}
+      <button class="btn" on:click={addPoint}>+1</button>
+      <button class="btn btn-dark" on:click={removePoint}>-1</button>
+      <input type="number" bind:value={points} />
+    {/if}
+  </div>
+</div>
